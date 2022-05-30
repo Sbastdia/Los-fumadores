5983c45af163cd13af97578abbc8147591d27fe3
Sbastdia <sbastdia@myuax.com>
2022-05-30 19:04:43 +0200

    actualizado el nombre de fósforos a cerillas

--- storage.py
+++ storage.py
@@ -1,11 +1,11 @@
 global store
 codes = ('1', '2', '3', '4', '5')
 store = {
-    '1': {'name': 'Papel', 'required': 'Tabaco, Fósforos, Filtros y Green', 'flag': False, 'request': None},
-    '2': {'name': 'Tabaco', 'required': 'Papel, Fósforos, Filtros y Green', 'flag': False, 'request': None},
-    '3': {'name': 'Fósforos', 'required': 'Papel, Tabaco, Filtros y Green', 'flag': False, 'request': None},
-    '4': {'name': 'Filtros', 'required': 'Papel, Tabaco, Fósforos y Green', 'flag': False, 'request': None},
-    '5': {'name': 'Green', 'required': 'Papel, Tabaco, Fósforos y Filtros', 'flag': False, 'request': None},
+    '1': {'name': 'Papel', 'required': 'Tabaco, Cerillas, Filtros y Green', 'flag': False, 'request': None},
+    '2': {'name': 'Tabaco', 'required': 'Papel, Cerillas, Filtros y Green', 'flag': False, 'request': None},
+    '3': {'name': 'Cerillas', 'required': 'Papel, Tabaco, Filtros y Green', 'flag': False, 'request': None},
+    '4': {'name': 'Filtros', 'required': 'Papel, Tabaco, Cerillas y Green', 'flag': False, 'request': None},
+    '5': {'name': 'Green', 'required': 'Papel, Tabaco, Cerillas y Filtros', 'flag': False, 'request': None},
 }
 
 time_smoke = 5
